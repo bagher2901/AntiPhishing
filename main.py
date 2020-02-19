8a062302a74194e68ce7d10ffd8e1c65510becd3
Chris Lovering <chris.lovering.95@gmail.com>
2020-02-19 18:54:39 +0000

    change how to change case

--- main.py
+++ main.py
@@ -5,29 +5,25 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+#Randomly changes the case of a word
+def change_case(word, rand_number=random.randint(0, 2):
+    if rand_number == 0:
+        return word.upper()
+    elif rand_number == 1:
+        return word.lower()
+    else:
+        return word.title()
 
-def generate_email(option=random.randint(0, 2)):
+
+def generate_email():
     # Opens the file, chooses a random line then strips any new lines at the end
     firstname = random.choice(open('firstnames.txt').readlines()).rstrip()
     lastname = random.choice(open('lastnames.txt').readlines()).rstrip()
     domain = random.choice(open('domains.txt').readlines()).rstrip()
-    
-    # Generate a random number to include in the email
-    number = str(random.randint(0, 500))
 
-    # Choose the case for the username based on the option parameter
-    if option == 0:
-        firstname = firstname.upper()
-        lastname = lastname.upper()
-    elif option == 1:
-        firstname = firstname.lower()
-        lastname = lastname.lower()
-    else:
-        firstname = firstname.title()
-        lastname = lastname.title()
-
-    # Join the username, shuffle it randomly and then return it with the domain appended
-    username = [firstname, lastname, number]
+    # Change the case of the names, add a random number,
+    # randomly shuffle the order and finally return it with the domain appended
+    username = [change_case(firstname), change_case(lastname), str(random.randint(0, 500))]
     random.shuffle(username)
     return ''.join(username) + domain
 
